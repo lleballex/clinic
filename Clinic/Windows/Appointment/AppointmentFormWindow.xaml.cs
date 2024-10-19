fcac4171dfc6d48d9a7741d6bb356f06a5bce4dd
lleballex <lleballex@gmail.com>
2024-10-19 10:25:41 +0300

    feat: add AppointmentFormWindow

---
+++ Clinic/Windows/Appointment/AppointmentFormWindow.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Clinic.Windows
+{
+    /// <summary>
+    /// Логика взаимодействия для AppointmentFormWindow.xaml
+    /// </summary>
+    public partial class AppointmentFormWindow : Window
+    {
+        public AppointmentFormWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
