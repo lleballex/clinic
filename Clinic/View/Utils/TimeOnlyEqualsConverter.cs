1d75af84245cc416595f2fe1c793b9ca4c4ca0e0
lleballex <lleballex@gmail.com>
2024-12-08 19:52:20 +0300

    refactor: move to separate file and rename TimeOnlyEqualsConverter

---
+++ Clinic/View/Utils/TimeOnlyEqualsConverter.cs
@@ -0,0 +1,25 @@
+﻿using System.Globalization;
+using System.Windows.Data;
+
+namespace Clinic.View.Utils
+{
+    public class TimeOnlyEqualsConverter : IMultiValueConverter 
+    {
+        public object Convert(object[] values, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (values[0] is TimeOnly && values[1] is TimeOnly)
+            {
+                if (((TimeOnly)values[0]).ToTimeSpan() == ((TimeOnly)values[1]).ToTimeSpan())
+                {
+                    return "true";
+                }
+            }
+            return "false";
+        }
+
+        public object[] ConvertBack(object value, Type[] targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
